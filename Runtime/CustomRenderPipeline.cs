6810c1e6aca8c9ae2ecad50933fecff5d5cdd6b2
yangtzehina <yangtzehina@users.noreply.github.com>
2020-08-08 22:44:22 +0800

    first commit

---
+++ Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomRP.Runtime
+{
+    public class CustomRenderPipeline : RenderPipeline
+    {
+        readonly CameraRenderer mRenderer =new CameraRenderer();
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            foreach (Camera camera in cameras)
+            {
+                mRenderer.Render(context,camera);
+            }
+        }
+    }
+}
